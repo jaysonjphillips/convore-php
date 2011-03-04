37949065af9ca878d4272486e1faa5288b9b3719
Jayson Phillips <jjp@jaysonjphillips.com>
2011-03-03 22:33:03 -0500

    refactored a bit. not happy yet

--- Convore.class.php
+++ Convore.class.php
@@ -2,41 +2,53 @@
 	
 	class Convore {
 		
-		private $username;
-		private $password;
+		private $credentials;
 		private $url_base;
 		
 		public function __construct($user, $pass) {
-			$this->username = $user;
-			$this->password = $pass;
-			
-			$this->url_base = "https://convore.com/api";
+			$this->credentials = sprintf('%s:%s', $user, $pass);
+			$this->url_base = "https://convore.com/api/";
 		}
 		
-		public function send_request($url, $action) {
+		public function sendRequest($url, $action) {
 			// extract curl request
 		}
 		
-		public function verify_account() {
-			$curl_conn = curl_init();
-			
-			//Set up the URL 
-			$verify = $this->url_base.'/account/verify.json';
-			
-			//Set cURL options
-			curl_setopt($curl_conn, CURLOPT_URL, $verify); 
-			curl_setopt($curl_conn, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($curl_conn, CURLOPT_USERPWD, $this->username.':'.$this->password); 
-			curl_setopt($curl_conn, CURLOPT_GET, 1); 
-			
-			$output = curl_exec($curl_conn);
-
-			curl_close($curl_conn);
-			
-			return $output;
-			
+		public function verifyAccount() {
+			return $this->methodCall('/account/verify.json', 'get');
 		}
 		
+		public function markAllRead() {
+			return $this->methodCall('/account/mark_read.json', 'get');
+		}
+		
+		public function usersOnline() {
+			return $this->methodCall('/account/online.json', 'get');
+		}
+		
+		private function methodCall($convore_method, $action, $auth_required = true, $params) {
+			$ch = curl_init();
+			
+			curl_setopt($ch, CURLOPT_URL, sprintf($this->base_url.'%s', $convore_method));
+			
+			if ($auth_required) {
+				curl_setopt($ch, CURLOPT_USERPWD, $this->credentials);
+			}
+			
+			if ($action == 'post' && !is_null($params)) {
+				curl_setopt($ch, CURLOPT_POST, true);
+		    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+			}
+			
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+				$convore_data = curl_exec($ch);
+				
+				$this->http_response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+				curl_close($ch);
+				return json_decode($convore_data);
+			}
+		}		
 	}
 	
 ?>
